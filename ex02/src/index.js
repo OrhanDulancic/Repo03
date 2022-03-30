24b45ec34b12bd97d228bcbbd7cf80e86fb01cf3
OrhanDulancic <orhan.dulancic@outlook.com>
2022-03-30 18:48:46 +0200

    Added js comments

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line  
+        /* This is multi-line 
+        comment! */
+        //This is single line comment
+    // Only change code above this line
+    
+}
+
+main();
+
+console.log(main());
+
+module.exports=main;
