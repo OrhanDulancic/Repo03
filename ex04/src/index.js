5091d425d2030d3d32a8b86bafd08983d8d0cf3a
OrhanDulancic <orhan.dulancic@outlook.com>
2022-03-30 19:32:40 +0200

    Declared a var x,y

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,8 +1,11 @@
 function main() {
     // Only change code below this line  
-        var forCat; 
+        var x;
+        x = 10;
+        var y;
+        y = x; 
     // Only change code above this line
-    
+    return [x,y]
 }
 
 console.log(main());
